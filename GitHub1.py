15d90520f5b927524a79758dbc2bf0e16d19b26d
AndriyUrban <90180554+AndriyUrban@users.noreply.github.com>
2021-09-11 15:00:41 +0300

    Add files via upload

---
+++ GitHub1.py
@@ -0,0 +1,14 @@
+import random
+#task 10
+#x=random.randint(0,50)
+#y=random.randint(2,5)
+#print(x**y)
+
+#task11
+#randnumb=random.uniform(1,10)
+#print(round(randnumb,2))
+
+#task12
+kg=int(input("enter your weight in kg: "))
+print(round(kg*2.2,-1))
+
