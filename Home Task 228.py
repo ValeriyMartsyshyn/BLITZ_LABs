ae47f715fee0a83c236b43571d6ef22950d1c152
AndriyUrban <90180554+AndriyUrban@users.noreply.github.com>
2021-09-20 09:37:32 +0300

    Add files via upload

---
+++ Home Task 228.py
@@ -0,0 +1,27 @@
+#import datetime
+ 
+#a = input('Перша дата (рік-місяць-день): ')
+#b = input('друга дата (рік-місяць-день): ')
+#a = a.split('-')
+#b = b.split('-')
+#a1 = datetime.date(int(a[0]),int(a[1]),int(a[2]))
+#b1 = datetime.date(int(b[0]),int(b[1]),int(b[2]))
+#c = a1-b1
+#print(c)
+
+#TASK11
+#lst=[]
+#a=("1","2","4")
+#for i in a:
+#    lst.append(i)
+#lst.reverse()
+#lst=tuple(lst)
+#print(lst)
+
+#TASK12
+l = ( ('a', 23), ('b', 37), ('c', 11), ('d', 29), )
+      
+print(sorted(l, key=lambda el: el[1])
+    
+    
+
