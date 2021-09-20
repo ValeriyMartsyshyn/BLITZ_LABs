cc96caf0d781044d2eb74ce6c6307dbee177128a
BodyaKharko <90180531+BodyaKharko@users.noreply.github.com>
2021-09-20 12:37:10 +0300

    Update Task[13-15].py

--- Task[13-15].py
+++ Task[13-15].py
@@ -1,20 +1,20 @@
 #task 13
 import math
-e=int(input("Enter an engle: "))
+e=float(input("Enter an engle: "))
 e1=math.radians(e)
 s=round(math.sin(e1),4)
 print("Sine is: ", end="")
 print(s)
   
 #task 14
-p=int(input("Enter a power: "))
+p=float(input("Enter a power: "))
 a=2**p
 s=str(a)[-2:]
 print ("Last two digits : ", end="")
 print (s)
 
 #task 15
-w=int(input("Enter a weight in kilograms: "))
+w=float(input("Enter a weight in kilograms: "))
 p=round((w*2.20462262185),1)
 print("In pounds: ", end="")
 print(p)
