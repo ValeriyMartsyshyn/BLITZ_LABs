c420f27a0b838c16ced0ce303809b15431025460
AndriyUrban <90180554+AndriyUrban@users.noreply.github.com>
2021-09-26 23:04:46 +0300

    Add files via upload

--- Task(10-12).py
+++ Task(10-12).py
@@ -1,13 +1,32 @@
-import random
-#task 10
-x=random.randint(0,50)
-y=random.randint(2,5)
-print(x**y)
+#Task 10
 
-#task11
-randnumb=random.uniform(1,10)
-print(round(randnumb,2))
+dct={'c': 2,
+     'r': 3,
+     't': 5,
+     'x': 21,
+     'y': 3,
+     'o': 54}
+lst_values=[i for i in dct.values()]
+max_value=max(lst_values)
+lst_keys=[i for i in dct.keys()]
+for i in lst_keys:
+    if dct[i]==max_value:
+        print('answer is: ' + i)
+#Task 11
+lst = list(input("Enter large int:"))
+lens = len(lst)
+n = -3
+c = 4
+while n>=-lens-150:
+    lst.insert(n, ',')
+    n-=c
+while lst[0] == ',':
+    lst.pop(0)
+k = ''.join(lst)
+print(k)
 
-#task12
-kg=float(input("enter your weight in kg: "))
-print(round(kg*2.2,1))
+#Task 12
+str='abcdefghijklmnopqrstuvwxyz'
+for i in range(0,26):
+    print(str[i:]+str[:i])
+
